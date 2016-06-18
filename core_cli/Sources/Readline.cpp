983c27be8285b39ef3f7fe28754c1d4439981ae7
Andrey <foursilico@gmail.com>
2016-06-18 23:44:34 +0300

    Add Readline class

---
+++ core_cli/Sources/Readline.cpp
@@ -0,0 +1,21 @@
+/*
+ * Readline.cpp
+ *
+ *  Created on: Jun 18, 2016
+ *      Author: avs
+ */
+
+#include "Readline.h"
+
+namespace Core {
+
+Readline::Readline() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Readline::~Readline() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Core */
---
+++ core_cli/Sources/Readline.h
@@ -0,0 +1,23 @@
+/*
+ * Readline.h
+ *
+ *  Created on: Jun 18, 2016
+ *      Author: avs
+ */
+
+#ifndef READLINE_H_
+#define READLINE_H_
+
+#include "Common.h"
+
+namespace Core {
+
+class Readline: public Uncopy {
+public:
+	Readline();
+	virtual ~Readline();
+};
+
+} /* namespace Core */
+
+#endif /* READLINE_H_ */
