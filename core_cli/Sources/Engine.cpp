0d11032d52fe8eb1e3cd572b106da67784f31e47
Andrey <foursilico@gmail.com>
2016-06-29 02:52:02 +0300

    Commit#5Linux hotfix for *core dumped*

--- core_cli/Sources/Engine.cpp
+++ core_cli/Sources/Engine.cpp
@@ -25,10 +25,10 @@ Engine::~Engine(void)
 
 void Engine::clear(void)
 {
-	delete Log::getSingletonPtr();
-	delete PathList::getSingletonPtr();
-	delete Runtime::getSingletonPtr();
-	delete CfgTable::getSingletonPtr();
+//	delete Log::getSingletonPtr();
+//	delete PathList::getSingletonPtr();
+//	delete Runtime::getSingletonPtr();
+//	delete CfgTable::getSingletonPtr();
 }
 
 void Engine::go(void)
