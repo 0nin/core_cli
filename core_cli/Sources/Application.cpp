f3f7394ad12fe1ce3bd592615ad0a52ac6473492
Andrey <foursilico@gmail.com>
2016-07-15 15:58:50 +0300

    Commit#2Win32

--- core_cli/Sources/Application.cpp
+++ core_cli/Sources/Application.cpp
@@ -40,27 +40,32 @@ unsigned plot(const std::vector<std::string> &)
 {
 	Gnuplot gp;
 
-	std::vector<std::pair<double, double> > xy_pts_A;
-	for (double x = -2; x < 2; x += 0.01)
-	{
-		double y = x * x * x;
-		xy_pts_A.push_back(std::make_pair(x, y));
-	}
+//	std::vector<std::pair<double, double> > xy_pts_A;
+//	for (double x = -2; x < 2; x += 0.01)
+//	{
+//		double y = x * x * x;
+//		xy_pts_A.push_back(std::make_pair(x, y));
+//	}
+//
+//	std::vector<std::pair<double, double> > xy_pts_B;
+//	for (double alpha = 0; alpha < 1; alpha += 1.0 / 24.0)
+//	{
+//		double theta = alpha * 2.0 * 3.14159;
+//		xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
+//	}
+//
+//	gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
+//	// Data will be sent via a temporary file.  These are erased when you call
+//	// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
+//	// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
+//	// and won't be deleted (this is useful when creating a script).
+//	gp << "plot" << gp.file1d(xy_pts_A) << "with lines title 'cubic',"
+//			<< gp.file1d(xy_pts_B) << "with points title 'circle'" << std::endl;
 
-	std::vector<std::pair<double, double> > xy_pts_B;
-	for (double alpha = 0; alpha < 1; alpha += 1.0 / 24.0)
-	{
-		double theta = alpha * 2.0 * 3.14159;
-		xy_pts_B.push_back(std::make_pair(cos(theta), sin(theta)));
-	}
-
-	gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
-	// Data will be sent via a temporary file.  These are erased when you call
-	// gp.clearTmpfiles() or when gp goes out of scope.  If you pass a filename
-	// (e.g. "gp.file1d(pts, 'mydata.dat')"), then the named file will be created
-	// and won't be deleted (this is useful when creating a script).
-	gp << "plot" << gp.file1d(xy_pts_A) << "with lines title 'cubic',"
-			<< gp.file1d(xy_pts_B) << "with points title 'circle'" << std::endl;
+	gp << "set key left box\n";
+	gp << "set autoscale\n";
+	gp << "set samples 800\n";
+	gp << "plot [-30:20] sin(x*20)*atan(x)\n";
 
 	return ret::Ok;
 }
