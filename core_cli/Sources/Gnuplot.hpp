f58a399216271fc2f98f47d1277b4536f728cbf8
Kalush_PC <Kalush_PC@Kalush_PC_PC>
2016-07-24 17:37:56 +0300

    Windows: add *.dll, some updated Signed-off-by: Kalush_PC <Kalush_PC@Kalush_PC_PC>

--- core_cli/Sources/Gnuplot.hpp
+++ core_cli/Sources/Gnuplot.hpp
@@ -13,9 +13,10 @@
 #include <iostream>
 
 #ifdef WIN32
-    #define GNUPLOT_NAME "pgnuplot -persist"
+//    #define GNUPLOT_NAME "pgnuplot -persist"
+#define GNUPLOT_NAME "gnuplot -persist"
 #else
-    #define GNUPLOT_NAME "gnuplot -persist"
+#define GNUPLOT_NAME "gnuplot -persist"
 #endif
 
 using std::string;
@@ -24,42 +25,43 @@ using std::cerr;
 class Gnuplot
 {
 public:
-    Gnuplot() ;
-    ~Gnuplot();
-    void operator ()(const string & command); // отправить команду gnuplot
+	Gnuplot();
+	~Gnuplot();
+	void operator ()(const string & command); // отправить команду gnuplot
 
 protected:
-    FILE *gnuplotpipe;
+	FILE *gnuplotpipe;
 };
 
 Gnuplot::Gnuplot()
 {
-    #ifdef WIN32
-        gnuplotpipe = _popen(GNUPLOT_NAME, "w");
-    #else
-        gnuplotpipe  = popen(GNUPLOT_NAME, "w");
-    #endif
+#ifdef WIN32
+	gnuplotpipe = _popen(GNUPLOT_NAME, "w");
+#else
+	gnuplotpipe = popen(GNUPLOT_NAME, "w");
+#endif
 
-    if (!gnuplotpipe)
-    {
-        cerr << ("Gnuplot not found !");
-    }
+	if (!gnuplotpipe)
+	{
+		cerr << ("Gnuplot not found !");
+	}
 }
 Gnuplot::~Gnuplot()
 {
-    fprintf(gnuplotpipe,"exit\n");
+	fprintf(gnuplotpipe, "exit\n");
 
-    #ifdef WIN32
-       _pclose(gnuplotpipe);
-    #else
-        pclose(gnuplotpipe);
-    #endif
+#ifdef WIN32
+	_pclose(gnuplotpipe);
+#else
+	pclose(gnuplotpipe);
+#endif
 }
 void Gnuplot::operator()(const string & command)
 {
-    fprintf(gnuplotpipe,"%s\n",command.c_str());
-    fflush(gnuplotpipe); //без fflush ничего рисоваться не будет
-};
+	fprintf(gnuplotpipe, "%s\n", command.c_str());
+	fflush(gnuplotpipe); //без fflush ничего рисоваться не будет
+}
+;
 
 //templat
 //void Gnuplot::operator()(const string & command)
